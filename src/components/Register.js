65af0d1a3b57b703091d7dd882bdd0abd0b66c5f
Ben Weissman <bweissman1@babson.edu>
2018-07-18 15:45:31 -0700

    Wednesday Register.js changes from Andrew

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,27 +1,78 @@
-import react from 'react';
+//REGISTER
 
-export default class Register extends React.Component{
+import React from 'react';
+import Login from './Login.js';
+
+export default class Register extends React.Component {
   constructor(props){
     super(props);
-    this.state={
+    this.state= {
       username: '',
-      password: ''
+      password: '',
+      confirmPwd: '',
     }
     this.registerUser = this.registerUser.bind(this);
   }
 
   registerUser(){
-    fetch('http:localhost:3000/register', {
-    method: "POST",
-    body: {
-      username: this.state.username,
-      password: this.state.password
+    if (this.state.password === this.state.confirmPwd){
+      console.log(this.state.username, this.state.password, this.state.confirmPwd)
+      return fetch('http://localhost:3000/register', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "same-origin",
+        body: JSON.stringify({
+          username: this.state.username,
+          password: this.state.password
+        })
+      })
+      .then(resp => {
+        if (resp.status === 200){
+          this.props.redirect('Login')
+        } else {
+          alert('did not work');
+        }
+      })
+      .catch(err => console.log(err))
+    } else {
+      alert('Passwords do not match')
     }
-  }).then(resp => {})
+  }
+
+onUsernameChange(e){
+  this.setState({
+    username: e.target.value
+  })
+}
+
+onPasswordChange(e){
+  this.setState({
+    password: e.target.value
+  })
+}
+
+onConPwdChange(e){
+  this.setState({
+    confirmPwd: e.target.value
+  })
 }
 
 render(){
   return(
+    <div>
+      <h1>Register Your Account</h1>
+      <form>
+        <input onChange={(e) => this.onUsernameChange(e)} placeholder='Username'/>
+        <br />
+        <input onChange={(e) => this.onPasswordChange(e)} placeholder='Password'/>
+        <br />
+        <input onChange={(e) => this.onConPwdChange(e)} placeholder='Confirm Password'/>
+        <button type='button' onClick={this.registerUser}></button>
+        <button onClick={() => this.props.redirect('Login')}></button>
+      </form>
+    </div>
   )
 }
 
